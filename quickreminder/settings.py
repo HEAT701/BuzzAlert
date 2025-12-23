d9bf2d440efd192a51328ef03c23c83f0d471545
jayram <jaybabu706146@gmail.com>
2025-12-23 12:20:52 +0530

    add cors headers

--- quickreminder/settings.py
+++ quickreminder/settings.py
@@ -50,9 +50,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'MessagesTemplates',
     'Send_Messages',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -62,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True 
 
 ROOT_URLCONF = 'quickreminder.urls'
 
